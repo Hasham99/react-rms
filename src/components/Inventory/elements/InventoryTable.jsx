0f671a938f6d74860fbbf2c2c5156d258650d3cf
Hasham <74524735+Hasham99@users.noreply.github.com>
2023-12-17 18:55:15 +0500

    inventory fetch

--- src/components/Inventory/elements/InventoryTable.jsx
+++ src/components/Inventory/elements/InventoryTable.jsx
@@ -12,107 +12,108 @@ import { useEffect, useState } from "react";
 
 const TABLE_HEAD = [
   "Product",
-  "Product ID",
+  // "Product ID",
   "Category",
-  "Location",
+  // "Location",
   "Available",
   "Reserved",
   "On Hand",
   "Actions",
 ];
 
-const TABLE_ROWS = [
-  {
-    name: "John Michael",
-    id: "Warehouse 1",
-    category: "Fast food",
-    location: "Warehouse 1",
-    available: "120",
-    reserved: "80",
-    onhand: "100",
-    actions: "actions",
-    icons: {},
-  },
-  {
-    name: "John Michael",
-    id: "Warehouse 1",
-    category: "Fast food",
-    location: "Warehouse 1",
-    available: "120",
-    reserved: "80",
-    onhand: "100",
-    actions: "actions",
-    icons: {},
-  },
-  {
-    name: "John Michael",
-    id: "Warehouse 1",
-    category: "Fast food",
-    location: "Warehouse 1",
-    available: "120",
-    reserved: "80",
-    onhand: "100",
-    actions: "actions",
-    icons: {},
-  },
-  {
-    name: "John Michael",
-    id: "Warehouse 1",
-    category: "Fast food",
-    location: "Warehouse 1",
-    available: "120",
-    reserved: "80",
-    onhand: "100",
-    actions: "actions",
-    icons: {},
-  },
-  {
-    name: "John Michael",
-    id: "Warehouse 1",
-    category: "Fast food",
-    location: "Warehouse 1",
-    available: "120",
-    reserved: "80",
-    onhand: "100",
-    actions: "actions",
-    icons: {},
-  },
-  {
-    name: "John Michael",
-    id: "Warehouse 1",
-    category: "Fast food",
-    location: "Warehouse 1",
-    available: "120",
-    reserved: "80",
-    onhand: "100",
-    actions: "actions",
-    icons: {},
-  },
-  {
-    name: "John Michael",
-    id: "Warehouse 1",
-    category: "Fast food",
-    location: "Warehouse 1",
-    available: "120",
-    reserved: "80",
-    onhand: "100",
-    actions: "actions",
-    icons: {},
-  },
-];
+// const TABLE_ROWS = [
+//   {
+//     name: "John Michael",
+//     id: "Warehouse 1",
+//     category: "Fast food",
+//     location: "Warehouse 1",
+//     available: "120",
+//     reserved: "80",
+//     onhand: "100",
+//     actions: "actions",
+//     icons: {},
+//   },
+//   {
+//     name: "John Michael",
+//     id: "Warehouse 1",
+//     category: "Fast food",
+//     location: "Warehouse 1",
+//     available: "120",
+//     reserved: "80",
+//     onhand: "100",
+//     actions: "actions",
+//     icons: {},
+//   },
+//   {
+//     name: "John Michael",
+//     id: "Warehouse 1",
+//     category: "Fast food",
+//     location: "Warehouse 1",
+//     available: "120",
+//     reserved: "80",
+//     onhand: "100",
+//     actions: "actions",
+//     icons: {},
+//   },
+//   {
+//     name: "John Michael",
+//     id: "Warehouse 1",
+//     category: "Fast food",
+//     location: "Warehouse 1",
+//     available: "120",
+//     reserved: "80",
+//     onhand: "100",
+//     actions: "actions",
+//     icons: {},
+//   },
+//   {
+//     name: "John Michael",
+//     id: "Warehouse 1",
+//     category: "Fast food",
+//     location: "Warehouse 1",
+//     available: "120",
+//     reserved: "80",
+//     onhand: "100",
+//     actions: "actions",
+//     icons: {},
+//   },
+//   {
+//     name: "John Michael",
+//     id: "Warehouse 1",
+//     category: "Fast food",
+//     location: "Warehouse 1",
+//     available: "120",
+//     reserved: "80",
+//     onhand: "100",
+//     actions: "actions",
+//     icons: {},
+//   },
+//   {
+//     name: "John Michael",
+//     id: "Warehouse 1",
+//     category: "Fast food",
+//     location: "Warehouse 1",
+//     available: "120",
+//     reserved: "80",
+//     onhand: "100",
+//     actions: "actions",
+//     icons: {},
+//   },
+// ];
 
 const InventoryTable = () => {
   const [inventoryData, setInventoryData] = useState([]);
   useEffect(() => {
     const fetchInventoryData = async () => {
       try {
-        const res = await fetch("http://52.90.182.126:3000/api/cai");
+        const res = await fetch("http://52.90.182.126:3000/api/inventory");
         const data = await res.json();
         setInventoryData(data);
       } catch (error) {
         console.log(error);
       }
     };
+    fetchInventoryData();
   }, []);
   return (
     <Card className=" w-full overflow-scroll">
@@ -136,7 +137,75 @@ const InventoryTable = () => {
           </tr>
         </thead>
         <tbody>
-          {TABLE_ROWS.map(
+          {inventoryData.map((data) => (
+            <tr key={data.inventory_id} className="even:bg-blue-gray-50/50">
+              <td className="p-4">
+                <Typography
+                  variant="small"
+                  color="blue-gray"
+                  className="font-normal"
+                >
+                  {data.item_name}
+                </Typography>
+              </td>
+              <td className="p-4">
+                <Typography
+                  variant="small"
+                  color="blue-gray"
+                  className="font-normal"
+                >
+                  {data.category_name}
+                </Typography>
+              </td>
+              <td className="p-4">
+                <Typography
+                  variant="small"
+                  color="blue-gray"
+                  className="font-normal"
+                >
+                  {data.available}
+                </Typography>
+              </td>
+              <td className="p-4">
+                <Typography
+                  variant="small"
+                  color="blue-gray"
+                  className="font-normal"
+                >
+                  {data.reserved}
+                </Typography>
+              </td>
+              <td className="p-4">
+                <Typography
+                  variant="small"
+                  color="blue-gray"
+                  className="font-normal"
+                >
+                  {data.on_hand}
+                </Typography>
+              </td>
+              <td className="p-2">
+                <Typography
+                  variant="small"
+                  color="blue-gray"
+                  className="font-normal"
+                >
+                  <Tooltip content="Edit User">
+                    <IconButton variant="text">
+                      <PencilSquareIcon className="h-4 w-4" />
+                    </IconButton>
+                  </Tooltip>
+                  <Tooltip content="Edit User">
+                    <IconButton variant="text">
+                      <TrashIcon className="h-4 w-4" />
+                    </IconButton>
+                  </Tooltip>
+                </Typography>
+              </td>
+            </tr>
+          ))}
+
+          {/* {TABLE_ROWS.map(
             (
               { name, id, category, location, available, reserved, onhand },
               index
@@ -223,20 +292,9 @@ const InventoryTable = () => {
                     </Tooltip>
                   </Typography>
                 </td>
-                {/* <td className="p-4">
-                  <Typography
-                    as="a"
-                    href="#"
-                    variant="small"
-                    color="blue-gray"
-                    className="font-medium"
-                  >
-                    Edit
-                  </Typography>
-                </td> */}
               </tr>
             )
-          )}
+          )} */}
         </tbody>
       </table>
     </Card>
